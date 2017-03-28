8b618227ce169c8e972c87f5fde6e0dd71dcff2d
oldreader <andrewserdyuk@gmail.com>
2017-03-28 23:36:55 +0700

    Initial commit

---
+++ first/src/main/java/company/Blah.java
@@ -0,0 +1,23 @@
+package company;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Bala
+public class Blah {
+
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Blah.class);
+
+    private Blah() {
+//        default
+    }
+
+    public static void main(String[] args) {
+        LOGGER.info("start");
+    }
+
+
+
+
+}
